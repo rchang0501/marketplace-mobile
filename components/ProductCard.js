800121de2e801368d0f36419e33131114777fe5c
Ryan Chang <ryanchang0501@gmail.com>
2022-04-18 19:15:16 -0400

    configured RectButton component in Button.js

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -57,7 +57,11 @@ const ProductCard = ({ data }) => {
           }}
         >
           <Price price={data.price} />
-          <RectButton />
+          <RectButton
+            minWidth={120}
+            fontSize={SIZES.font}
+            handlePress={() => navigation.navigate("Details", { data })}
+          />
         </View>
       </View>
     </View>
