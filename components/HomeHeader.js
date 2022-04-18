e06d8b3d536f184551d0edad36dfbc9021bbba2a
Ryan Chang <ryanchang0501@gmail.com>
2022-04-18 19:52:48 -0400

    integrated search bar in HomeHeader

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -42,22 +42,50 @@ const HomeHeader = () => {
           />
         </View>
       </View>
-      <View style={{
-        marginVertical: SIZES.font
-      }} >
-        <Text style={{fontFamily:FONTS.regular, fontSize:SIZES.small, color:COLORS.white}} >
-          Hello, Ryan  👋
+      <View
+        style={{
+          marginVertical: SIZES.font,
+        }}
+      >
+        <Text
+          style={{
+            fontFamily: FONTS.regular,
+            fontSize: SIZES.small,
+            color: COLORS.white,
+          }}
+        >
+          Glad you're here, Ryan 🙌
         </Text>
-        <Text style={{fontFamily:FONTS.bold, fontSize:SIZES.large, color:COLORS.white, marginTop: SIZES.base/2}} >
-          Let's find a masterpiece
+        <Text
+          style={{
+            fontFamily: FONTS.bold,
+            fontSize: SIZES.large,
+            color: COLORS.white,
+            marginTop: SIZES.base / 2,
+          }}
+        >
+          Let's do something special!
         </Text>
       </View>
-      <View style={{marginTop: SIZES.font}} >
-        <View>
-          <Image 
+      <View style={{ marginTop: SIZES.font }}>
+        <View style={{
+          width:'100%',
+          borderRadius: SIZES.font,
+          backgroundColor:COLORS.gray,
+          flexDirection:'row',
+          alignItems:'center',
+          paddingHorizontal:SIZES.font,
+          paddingVertical:SIZES.small-2
+        }} >
+          <Image
             source={assets.search}
             resizeMode="contain"
-            style={{width: 20, height: 20, marginRight: SIZES.base}}
+            style={{ width: 20, height: 20, marginRight: SIZES.base }}
+          />
+          <TextInput
+            placeholder="Search Products"
+            style={{ flex: 1 }}
+            onChangeText={() => {}}
           />
         </View>
       </View>
