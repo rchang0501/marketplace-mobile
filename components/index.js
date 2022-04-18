5c2ab141129c169e45d9f813857a094f8726a3d0
Ryan Chang <ryanchang0501@gmail.com>
2022-04-18 17:08:03 -0400

    created react components and sample data

---
+++ components/index.js
@@ -0,0 +1,5 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import ProductCard from "./ProductCard";
+
+export { HomeHeader, FocusedStatusBar, ProductCard };
--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,16 @@
-import { Text } from "react-native";
+import { useState } from "react";
+import { View, SafeAreaView, FlatList, Text } from "react-native"; // FlatList is recycler view, SafeArea renders content in safe area boundary to make sure things fit on the screen, View is a div
 
+import { COLORS, NFTData } from '../constants'
+import { ProductCard, HomeHeader, FocusedStatusBar } from "../components";
+
+// SafeAreaView is like the outer div
 const Home = () => {
-    return <Text>Home</Text>;
-}
+  return (
+      <SafeAreaView style = {{flex:1}}> 
+        
+      </SafeAreaView>
+  )
+};
 
-export default Home
+export default Home;
